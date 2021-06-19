562587c941307b46bc36a6537e6eb3ec5e855e88
Ian Figueiredo <iandlf2347@gmail.com>
2021-06-18 22:58:38 -0300

    Adicionado tabuleiro e input do usuário

---
+++ game.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void showGrid(char grid[]){
+    for(int i = 0; i<9; i ++){
+        if(i == 2 || i == 5){
+            printf("%c\n", grid[i]);
+        } else{
+            printf("%c  ", grid[i]);
+        }
+    }
+}
+
+bool win(char grid[]){
+    if(grid[0] == grid[1] == grid[2] || grid[3] == grid[4] == grid[5] || grid[6] == grid[7] == grid[8] || grid[0] == grid[4] == grid[8] || grid[2] == grid[4] == grid[6] || grid[0] == grid[3] == grid[6] || grid[1] == grid[4] == grid[7] || grid[2] == grid[5] == grid[8]) {
+        return true;
+    }
+}
---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "game.h"
+
+int main(){
+    int turns = 0;
+    char grid[] = {
+                    '1', '2', '3', 
+                    '4', '5', '6', 
+                    '7', '8', '9'};
+    
+    system("cls");
+    showGrid(grid);
+
+    while(1==1){
+        int pos;
+
+        if(turns % 2 == 0){
+
+            printf("\nIn what position do you want to put the X? ");
+            scanf("%d", &pos);
+            grid[pos-1] = 'X';
+
+        } else{
+            
+            printf("\nIn what position do you want to put the O? ");
+            scanf("%d", &pos);
+            grid[pos-1] = 'O';
+
+        }
+
+        
+        
+
+
+        system("cls");
+        showGrid(grid);
+        turns++;
+    }
+
+
+    return 0;
+}
